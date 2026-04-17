b506b834eb6c4544cc9ca64997dace8dfb0cbbd2
bkase-dotcom <bkase@inside.artcenter.edu>
2026-04-17 14:17:31 -0700

    Initial Commit

---
+++ Holistic/HolisticApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolisticApp.swift
+//  Holistic
+//
+//  Created by Benjamin Kase on 4/17/26.
+//
+
+import SwiftUI
+
+@main
+struct HolisticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HolisticTests/HolisticTests.swift
@@ -0,0 +1,19 @@
+//
+//  HolisticTests.swift
+//  HolisticTests
+//
+//  Created by Benjamin Kase on 4/17/26.
+//
+
+import Testing
+@testable import Holistic
+
+struct HolisticTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
